8aa7ee9d61bbc43e0e11d05cadedb928529b9425
TZ\Tz <tienn.hcmus@gmail.com>
2020-08-21 10:45:52 +0700

    app connection and send message

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const http = require("http").Server(app);
+const port = 3000;
+const io = require("socket.io")(http);
 
-app.get('/hello', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/hello", (req, res) => {
+  res.send("Hello World!");
+});
 
-app.get('/', function(req, res){
-  res.sendFile('/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
   });
+});
 
-app.listen(port, () => {
-  console.log(`Chating application listening at http://localhost:${port}`)
-})
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg); 
+  });
+});
+
+http.listen(port, () => {
+  console.log(`Chating application listening at http://localhost:${port}`);
+});
